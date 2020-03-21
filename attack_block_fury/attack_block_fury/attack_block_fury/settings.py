8c8d56e81f489ffa942b1b6602ae453399c3d52f
Tyler-Jay-Stevahn <tylerjay1996@hotmail.com>
2020-03-21 16:41:48 -0500

    added requirements file

--- attack_block_fury/attack_block_fury/attack_block_fury/settings.py
+++ attack_block_fury/attack_block_fury/attack_block_fury/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'main',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -75,10 +76,15 @@ WSGI_APPLICATION = 'attack_block_fury.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'des7pj7e0vhh1j', 
+        'USER': 'vjcxsbybnruaap',
+        'PASSWORD': 'b53ae461a19b2817c4354a30270cb69517671fc99c6d67d68f43c0c6e91eabfd',
+        'HOST':  'ec2-52-207-93-32.compute-1.amazonaws.com',
+        'PORT':  '5432',
+
     }
-}
+}}
 
 
 # Password validation
@@ -117,4 +123,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+import django_heroku
+django_heroku.settings(locals())
