a8695fa5468159e95d20aef5b3f8a970e6a1cf74
Tyler-Jay-Stevahn <tylerjay1996@hotmail.com>
2020-03-22 14:49:37 -0500

    testing mobile phone connections.

--- attack_block_fury/main/views.py
+++ attack_block_fury/main/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from .models import twilio
 from django_twilio.decorators import twilio_view
 from twilio.twiml.messaging_response import MessagingResponse
-from send_to_user import send_it
+from .send_to_user import send_it
 
 # Create your views here.
 
